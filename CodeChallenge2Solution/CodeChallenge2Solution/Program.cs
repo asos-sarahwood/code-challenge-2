31e6a34a59cf58bb4ec4ad836307bf7cd0c972bc
Sarah Wood <sarah.wood@asos.com>
2018-02-05 12:40:29 +0000

    Cleaned up old methods

--- CodeChallenge2Solution/CodeChallenge2Solution/Program.cs
+++ CodeChallenge2Solution/CodeChallenge2Solution/Program.cs
@@ -22,58 +22,10 @@ namespace CodeChallenge2Solution
 
         public static void Main()
         {
-            GetNumberOfLines();
-            //BusiestHour();
             IterateThroughLogFile();
             Console.ReadLine();
-
         }
-
-        public static void ReadLines()
-        {
-            var readLines = File.ReadLines(logFile);
-        }
-
-        private static void GetNumberOfLines()
-        {
-            var numberOfLines = File.ReadAllLines(logFile).Count();
-            Console.WriteLine($"there are {numberOfLines} lines in this file ");
-
-        }
-
-        public static void BusiestHour()
-        {
-            var readLines = File.ReadLines(logFile);
-            List<string> allDatesInLogFile = new List<string>();
-            List<string> allTimesInLogFile = new List<string>();
-            List<string> allHoursInLogFile = new List<string>();
-
-            foreach (var line in readLines)
-            {
-                if (line.StartsWith("2018"))
-                {
-
-                    string[] splitBySpace = line.Split(' ');
-                    string date = splitBySpace[0];
-                    string time = splitBySpace[1];
-                    allDatesInLogFile.Add(date);
-                    allTimesInLogFile.Add(time);
-                    string[] splitByColon = time.Split(':');
-                    string hourLogged = splitByColon[0];
-                    allHoursInLogFile.Add(hourLogged);
-
-                    Console.WriteLine($"there are {date} lines in this file at {hourLogged} ");
-                }
-
-
-
-            }
-
-        }
-
-
         public class EventLogEntry
-
         {
             public string date { get; set; }
             public string time { get; set; }
@@ -104,21 +56,12 @@ namespace CodeChallenge2Solution
                 
                 foreach (var time in times)
                 {
-                    var hoursLoggedPerDate = time.Key;
+                    var hourOfGroupedLogs = time.Key;
                     var countOfLogsByHour = time.Count();
-                   
-                   // var counter = Enumerable.Repeat('#', time.Count());
-                    
-                        
+                    var counter = string.Concat(Enumerable.Repeat('#', time.Count()));
 
-                 //   Console.WriteLine($"for the hour {hoursLoggedPerDate} there were {countOfLogsByHour} number of Logs");
-                    Console.WriteLine($"{hoursLoggedPerDate}:  {string.Concat(Enumerable.Repeat('#', time.Count()))}  {countOfLogsByHour}");
-                 
-
-                 
-
+                    Console.WriteLine($"{hourOfGroupedLogs}:  {counter}  {countOfLogsByHour}");
                 }
-               
             }
         }
     }
